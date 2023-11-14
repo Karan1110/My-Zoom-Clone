f24c732425aa483b098e568c2de08a0528a7d659
Karan1110 <karancsipsvig5@gmail.com>
2023-11-14 21:53:51 +0530

    sss

--- public/script.js
+++ public/script.js
@@ -1,102 +1,68 @@
-// Initialize socket.io
 const socket = io("/")
-
-// Get a reference to the video grid
 const videoGrid = document.getElementById("video-grid")
-
-// Initialize a Peer object for WebRTC communication
 const myPeer = new Peer(undefined, {
-  host: "/", // The host should be the server where your Peer server is running
-  port: "42070", // The port for the Peer server
+  host: "/",
+  port: "42070",
 })
-
-// Create a video element for the local user's video
 const myVideo = document.createElement("video")
-myVideo.muted = true // Mute the local user's video
-
-// Get a reference to the microphone button and add a click event listener
-const mic = document.getElementById("mic")
-mic.addEventListener("click", () => {
-  myVideo.muted = !myVideo.muted // Toggle audio mute/unmute
-})
-
-// Initialize an empty object to store connected peers
+myVideo.title = "titlo"
+myVideo.muted = true
 const peers = {}
-
-// Get user media (audio and video)
 navigator.mediaDevices
   .getUserMedia({
     video: true,
     audio: true,
   })
   .then((stream) => {
-    // Add the local user's video stream to the grid
+    if (!myVideo.muted) {
+      stream.getVideoTracks().forEach((track) => {
+        track.enabled = false
+      })
+    }
+    stream.getAudioTracks().forEach((track) => {
+      track.enabled = false
+    })
     addVideoStream(myVideo, stream)
 
-    // Handle incoming calls
     myPeer.on("call", (call) => {
-      call.answer(stream) // Answer the call with the local stream
+      call.answer(stream)
       const video = document.createElement("video")
       call.on("stream", (userVideoStream) => {
         addVideoStream(video, userVideoStream)
       })
     })
 
-    // Listen for user-connected events
     socket.on("user-connected", (userId) => {
-      connectToNewUser(userId, stream)
+      // user is joining`
+      setTimeout(() => {
+        // user joined
+        connectToNewUser(userId, stream)
+      }, 2000)
     })
   })
 
-// Listen for cam-off events from other users
-socket.on("cam-off", (userId) => {
-  const userCam = document.getElementById(`video-${userId}`)
-  // Replace the video element with a message indicating the camera is off
-  if (userCam) {
-    userCam.replaceWith(
-      (document.createElement("p").textContent = `${userId} cam off`)
-    )
-  }
-})
-
-// Listen for user-disconnected events
 socket.on("user-disconnected", (userId) => {
-  if (peers[userId]) {
-    peers[userId].close() // Close the Peer connection
-  }
+  if (peers[userId]) peers[userId].close()
 })
 
-// Handle the "open" event from myPeer
 myPeer.on("open", (id) => {
-  socket.emit("join-room", ROOM_ID, id) // Emit a "join-room" event with the room ID and peer ID
+  socket.emit("join-room", ROOM_ID, id)
 })
 
-let userId // Variable to store the user's ID
-
-// Function to connect to a new user
 function connectToNewUser(userId, stream) {
-  userId = userId
-  const call = myPeer.call(userId, stream) // Call the new user with the local stream
+  const call = myPeer.call(userId, stream)
   const video = document.createElement("video")
-  video.id = `video-${userId}`
+  video.title = "titlo"
   call.on("stream", (userVideoStream) => {
     addVideoStream(video, userVideoStream)
   })
   call.on("close", () => {
-    video.style.display = "none"
     video.remove()
   })
 
-  peers[userId] = call // Store the call object in the peers object
+  peers[userId] = call
 }
 
-// Get a reference to the camera button and add a click event listener
-const cam = document.getElementById("cam")
-cam.addEventListener("click", () => {
-  socket.emit("cam-off", userId) // Emit a "cam-off" event with the user's ID
-})
-
-// Function to add a video stream to the grid
 function addVideoStream(video, stream) {
   video.srcObject = stream
   video.addEventListener("loadedmetadata", () => {
--- server.js
+++ server.js
@@ -1,27 +1,27 @@
-const express = require('express')
+const express = require("express")
 const app = express()
-const server = require('http').Server(app)
-const io = require('socket.io')(server)
-const { v4: uuidV4 } = require('uuid')
+const server = require("http").Server(app)
+const io = require("socket.io")(server)
+const { v4: uuidV4 } = require("uuid")
 
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
+app.set("view engine", "ejs")
+app.use(express.static("public"))
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.redirect(`/${uuidV4()}`)
 })
 
-app.get('/:room', (req, res) => {
-  res.render('room', { roomId: req.params.room })
+app.get("/:room", (req, res) => {
+  res.render("room", { roomId: req.params.room })
 })
 
-io.on('connection', socket => {
-  socket.on('join-room', (roomId, userId) => {
+io.on("connection", (socket) => {
+  socket.on("join-room", (roomId, userId) => {
     socket.join(roomId)
-    socket.to(roomId).broadcast.emit('user-connected', userId)
+    socket.to(roomId).broadcast.emit("user-connected", userId)
 
-    socket.on('disconnect', () => {
-      socket.to(roomId).broadcast.emit('user-disconnected', userId)
+    socket.on("disconnect", () => {
+      socket.to(roomId).broadcast.emit("user-disconnected", userId)
     })
   })
 })
